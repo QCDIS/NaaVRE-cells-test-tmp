a2ac5850ed50193b36d45ee2c12d419bce2adfa1
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-02-20 13:27:44 +0100

    create-file-dev-user-name-at-domain-com creation

---
+++ create-file-dev-user-name-at-domain-com/create-file-dev-user-name-at-domain-com.py
@@ -0,0 +1,39 @@
+from os.path import isfile
+from os.path import join
+from os import listdir
+import os
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+conf_data_folder = os.path.join('/tmp','data')
+
+
+conf_data_folder = os.path.join('/tmp','data')
+
+L = ["a\n", "b\n", "c\n"]
+file_path =  os.path.join(conf_data_folder,'hello.txt')
+fp = open(file_path, 'w')
+fp.writelines(L)
+fp.close()
+
+onlyfiles = [f for f in listdir(conf_data_folder) if isfile(join(conf_data_folder, f))]
+
+print(onlyfiles)
+
+import json
+filename = "/tmp/file_path_" + id + ".json"
+file_file_path = open(filename, "w")
+file_file_path.write(json.dumps(file_path))
+file_file_path.close()
