fdf32b185c35783478ac21ac2516cfdc79f989be
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-02-20 13:27:32 +0100

    create-file-r-dev-user-name-at-domain-com creation

---
+++ create-file-r-dev-user-name-at-domain-com/create-file-r-dev-user-name-at-domain-com.R
@@ -0,0 +1,42 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+
+
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+
+id <- gsub('"', '', opt$id)
+
+
+
+
+
+
+L <- c("a", "b", "c")
+
+conf_data_folder <- "/tmp/data"
+file_path <- file.path(conf_data_folder, "hello.txt")
+
+writeLines(L, file_path)
+
+onlyfiles <- list.files(conf_data_folder, full.names = TRUE)
+
+print(onlyfiles)
+
+
+
+# capturing outputs
+file <- file(paste0('/tmp/file_path_', id, '.json'))
+writeLines(toJSON(file_path, auto_unbox=TRUE), file)
+close(file)
