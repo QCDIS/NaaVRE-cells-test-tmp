5966bcce8447aca7229a69a1c70193434e65bbf3
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-02-20 13:28:19 +0100

    loop-list-dev-user-name-at-domain-com creation

---
+++ loop-list-dev-user-name-at-domain-com/loop-list-dev-user-name-at-domain-com.py
@@ -0,0 +1,24 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--list_of_paths', action='store', type=str, required=True, dest='list_of_paths')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+import json
+list_of_paths = json.loads(args.list_of_paths)
+
+
+
+
+for l in list_of_paths:
+    print(l)
+
