0260a4766e64d55c491eaf0891bb9b8a8f692078
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-02-20 13:27:42 +0100

    input-lists-r-dev-user-name-at-domain-com creation

---
+++ input-lists-r-dev-user-name-at-domain-com/input-lists-r-dev-user-name-at-domain-com.R
@@ -0,0 +1,46 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+
+
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+
+id <- gsub('"', '', opt$id)
+
+
+
+
+
+
+list_of_paths <- c(
+  "/webdav/LAZ/targets_myname",
+  "/webdav/LAZ/targets_myname",
+  "/webdav/LAZ/targets_myname",
+  "/webdav/LAZ/targets_myname"
+)
+
+list_of_ints <- c(1, 2, 35, 6, 65)
+
+print(list_of_paths)
+print(list_of_ints)
+
+
+
+# capturing outputs
+file <- file(paste0('/tmp/list_of_paths_', id, '.json'))
+writeLines(toJSON(list_of_paths, auto_unbox=TRUE), file)
+close(file)
+file <- file(paste0('/tmp/list_of_ints_', id, '.json'))
+writeLines(toJSON(list_of_ints, auto_unbox=TRUE), file)
+close(file)
