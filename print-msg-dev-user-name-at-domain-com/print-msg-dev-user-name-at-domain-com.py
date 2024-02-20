67f4df17921ce2fa54f263b02502ea0ff076deae
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-02-20 13:28:14 +0100

    print-msg-dev-user-name-at-domain-com creation

---
+++ print-msg-dev-user-name-at-domain-com/print-msg-dev-user-name-at-domain-com.py
@@ -0,0 +1,21 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--msg', action='store', type=int, required=True, dest='msg')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+msg = args.msg
+
+
+
+print('msg:', str(msg))
+
