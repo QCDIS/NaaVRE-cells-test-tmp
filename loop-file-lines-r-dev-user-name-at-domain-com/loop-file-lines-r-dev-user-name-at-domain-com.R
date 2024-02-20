8bed93078c10a964c0a8a62818f21e38c21c1f01
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-02-20 13:28:02 +0100

    loop-file-lines-r-dev-user-name-at-domain-com creation

---
+++ loop-file-lines-r-dev-user-name-at-domain-com/loop-file-lines-r-dev-user-name-at-domain-com.R
@@ -0,0 +1,38 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+
+
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--lines"), action="store", default=NA, type="character", help="my description")
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+
+id <- gsub('"', '', opt$id)
+lines = fromJSON(opt$lines)
+
+
+
+
+
+count <- 0
+for (l in lines) {
+    count <- count + 1
+    cat(sprintf("Line %d: %s\n", count, trimws(l)))
+}
+
+
+
+# capturing outputs
+file <- file(paste0('/tmp/count_', id, '.json'))
+writeLines(toJSON(count, auto_unbox=TRUE), file)
+close(file)
