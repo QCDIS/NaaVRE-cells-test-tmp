a3d5fe8bac471ec3d7011ea570ccc2a4a3218eac
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-02-22 15:48:58 +0100

    add1-dev-user-name-at-domain-com creation

---
+++ add1-dev-user-name-at-domain-com/task.py
@@ -0,0 +1,27 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--count', action='store', type=int, required=True, dest='count')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+count = args.count
+
+
+
+
+a = count + 1
+
+import json
+filename = "/tmp/a_" + id + ".json"
+file_a = open(filename, "w")
+file_a.write(json.dumps(a))
+file_a.close()
