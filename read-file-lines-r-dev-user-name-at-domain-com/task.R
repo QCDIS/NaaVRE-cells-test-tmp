531c0db5243d18449bc8c97f871da5a649264606
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-02-22 15:48:18 +0100

    read-file-lines-r-dev-user-name-at-domain-com creation

---
+++ read-file-lines-r-dev-user-name-at-domain-com/task.R
@@ -0,0 +1,43 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+
+
+option_list = list(
+
+make_option(c("--file_path"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+
+file_path <- gsub('"', '', opt$file_path)
+id <- gsub('"', '', opt$id)
+
+
+
+conf_data_folder <- file.path('/tmp', 'data')
+
+
+conf_data_folder <- file.path('/tmp', 'data')
+
+onlyfiles <- list.files(conf_data_folder, full.names = TRUE)
+
+print(onlyfiles)
+
+f <- file(file_path, "r")
+lines <- readLines(f)
+close(f)
+
+
+
+# capturing outputs
+file <- file(paste0('/tmp/lines_', id, '.json'))
+writeLines(toJSON(lines, auto_unbox=TRUE), file)
+close(file)
