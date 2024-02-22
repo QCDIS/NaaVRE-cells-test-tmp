83f63f6d289027899a75856bbe5f9234eca00ad6
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-02-22 15:48:38 +0100

    loop-int-list-r-dev-user-name-at-domain-com creation

---
+++ loop-int-list-r-dev-user-name-at-domain-com/task.R
@@ -0,0 +1,33 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+
+
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--list_of_ints"), action="store", default=NA, type="character", help="my description")
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+
+id <- gsub('"', '', opt$id)
+list_of_ints = fromJSON(opt$list_of_ints)
+
+
+
+
+
+
+for (l in list_of_ints) {
+    print(l)
+}
+
+
+
