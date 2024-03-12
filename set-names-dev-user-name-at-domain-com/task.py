857c379bfb37e4a12e078ecb36298dbd9e3e67a0
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-03-12 14:55:14 +0100

    set-names-dev-user-name-at-domain-com creation

---
+++ set-names-dev-user-name-at-domain-com/task.py
@@ -0,0 +1,23 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+
+names = ['Alice', 'Bob']
+
+import json
+filename = "/tmp/names_" + id + ".json"
+file_names = open(filename, "w")
+file_names.write(json.dumps(names))
+file_names.close()
