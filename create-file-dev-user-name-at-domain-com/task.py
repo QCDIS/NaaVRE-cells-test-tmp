b0d10cd5cb267b0aeca48414e6a696459f90eb0c
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-03-12 13:22:36 +0100

    create-file-dev-user-name-at-domain-com update

--- create-file-dev-user-name-at-domain-com/task.py
+++ create-file-dev-user-name-at-domain-com/task.py
@@ -31,6 +31,7 @@ fp.close()
 onlyfiles = [f for f in listdir(conf_data_folder) if isfile(join(conf_data_folder, f))]
 
 print(onlyfiles)
+a = 0.32757265208721675
 
 import json
 filename = "/tmp/file_path_" + id + ".json"
