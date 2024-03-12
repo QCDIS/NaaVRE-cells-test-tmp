4d9427b4f64a522ee062ad694b7c722776a8d402
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-03-12 11:45:29 +0100

    create-file-r-dev-user-name-at-domain-com update

--- create-file-r-dev-user-name-at-domain-com/task.R
+++ create-file-r-dev-user-name-at-domain-com/task.R
@@ -33,9 +33,6 @@ writeLines(L, file_path)
 onlyfiles <- list.files(conf_data_folder, full.names = TRUE)
 
 print(onlyfiles)
-abcdefghijklmnopqrstuvwxyz = 1
-abcdefghijklmnopqrstuvwxyz = 1
-abcdefghijklmnopqrstuvwxyz = 1
 
 
 
