c437c4321426510ecf5530e1a87f69db2843e74b
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-03-12 11:13:43 +0100

    consume-climwin-dev-user-name-at-domain-com update

--- consume-climwin-dev-user-name-at-domain-com/task.R
+++ consume-climwin-dev-user-name-at-domain-com/task.R
@@ -17,6 +17,7 @@ library(zoo)
 option_list = list(
 
 make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
 make_option(c("--rolling_mean_temp_str"), action="store", default=NA, type="character", help="my description"), 
 make_option(c("--temperature_data_str"), action="store", default=NA, type="character", help="my description")
 
@@ -26,6 +27,7 @@ make_option(c("--temperature_data_str"), action="store", default=NA, type="chara
 opt = parse_args(OptionParser(option_list=option_list))
 
 
+id <- gsub('"', '', opt$id)
 id <- gsub('"', '', opt$id)
 rolling_mean_temp_str <- gsub('"', '', opt$rolling_mean_temp_str)
 temperature_data_str <- gsub('"', '', opt$temperature_data_str)
@@ -37,6 +39,7 @@ temperature_data_str <- gsub('"', '', opt$temperature_data_str)
 
 cat("Original Temperature Data:\n", head(temperature_data_str), "\n\n")
 cat("Rolling Mean Temperature in Moving Windows:\n", head(coredata(rolling_mean_temp_str)), "\n")
+abcdefghijklmnopqrstuvwxyz=1
 
 
 
