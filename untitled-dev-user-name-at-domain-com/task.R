d6bffe3b9c86c4c96f1e2667cf9f14b3ccc33dec
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-03-19 10:55:04 +0100

    untitled-dev-user-name-at-domain-com creation

---
+++ untitled-dev-user-name-at-domain-com/task.R
@@ -0,0 +1,31 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+
+
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--param_mylist"), action="store", default=NA, type="character", help="my description")
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+id <- gsub('"', '', opt$id)
+
+param_mylist = opt$param_mylist
+
+
+
+
+for (item in param_mylist) {
+    print(item)
+}
+
+
+
