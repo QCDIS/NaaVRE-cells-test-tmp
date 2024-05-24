f16a50961409c5a3bd7f1c9cbc46d90ce785237b
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-24 13:22:37 +0200

    anti-pattern-dev-user-name-at-domain-com update

--- anti-pattern-dev-user-name-at-domain-com/task.py
+++ anti-pattern-dev-user-name-at-domain-com/task.py
@@ -1,5 +1,6 @@
 
 import argparse
+import json
 arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
@@ -24,8 +25,6 @@ some_list = range(count, a+1)
 
 msg = '1'
 
-import json
-filename = "/tmp/msg_" + id + ".json"
-file_msg = open(filename, "w")
+file_msg = open("/tmp/msg_" + id + ".json", "w")
 file_msg.write(json.dumps(msg))
 file_msg.close()
