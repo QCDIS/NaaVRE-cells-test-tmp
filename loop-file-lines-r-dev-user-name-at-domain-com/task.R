9beec22212784a48b094f67c38d1dc4aab97a557
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-24 13:22:28 +0200

    loop-file-lines-r-dev-user-name-at-domain-com update

--- loop-file-lines-r-dev-user-name-at-domain-com/task.R
+++ loop-file-lines-r-dev-user-name-at-domain-com/task.R
@@ -1,37 +1,76 @@
 setwd('/app')
-
-# retrieve input parameters
-
 library(optparse)
 library(jsonlite)
 
 
+
+print('option_list')
 option_list = list(
 
 make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
 make_option(c("--lines"), action="store", default=NA, type="character", help="my description")
-
 )
 
-# set input parameters accordingly
+
 opt = parse_args(OptionParser(option_list=option_list))
 
-id <- gsub('"', '', opt$id)
-lines = fromJSON(opt$lines)
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+print("Retrieving lines")
+var = opt$lines
+print(var)
+var_len = length(var)
+print(paste("Variable lines has length", var_len))
+
+print("------------------------Running var_serialization for lines-----------------------")
+print(opt$lines)
+lines = var_serialization(opt$lines)
+print("---------------------------------------------------------------------------------")
 
 
 
-
-
+print("Running the cell")
 count <- 0
 for (l in lines) {
     count <- count + 1
     cat(sprintf("Line %d: %s\n", count, trimws(l)))
 }
-
-
-
 # capturing outputs
+print('Serialization of count')
 file <- file(paste0('/tmp/count_', id, '.json'))
 writeLines(toJSON(count, auto_unbox=TRUE), file)
 close(file)
