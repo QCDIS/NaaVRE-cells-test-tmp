6f6735710de0be9c8bbafb74f8d915a9f07a792c
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-24 13:22:33 +0200

    add1-dev-user-name-at-domain-com update

--- add1-dev-user-name-at-domain-com/task.py
+++ add1-dev-user-name-at-domain-com/task.py
@@ -1,5 +1,6 @@
 
 import argparse
+import json
 arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
@@ -20,8 +21,6 @@ count = args.count
 
 a = count + 1
 
-import json
-filename = "/tmp/a_" + id + ".json"
-file_a = open(filename, "w")
+file_a = open("/tmp/a_" + id + ".json", "w")
 file_a.write(json.dumps(a))
 file_a.close()
