147b28c33b0290c8c28997821f8d768e1175cf6f
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-24 13:21:27 +0200

    input-lists-dev-user-name-at-domain-com update

--- input-lists-dev-user-name-at-domain-com/task.py
+++ input-lists-dev-user-name-at-domain-com/task.py
@@ -1,5 +1,6 @@
 
 import argparse
+import json
 arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
@@ -22,12 +23,9 @@ list_of_paths = ["/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/web
 list_of_ints = [1,2,35,6,65]
 print(msg)
 
-import json
-filename = "/tmp/list_of_paths_" + id + ".json"
-file_list_of_paths = open(filename, "w")
+file_list_of_paths = open("/tmp/list_of_paths_" + id + ".json", "w")
 file_list_of_paths.write(json.dumps(list_of_paths))
 file_list_of_paths.close()
-filename = "/tmp/list_of_ints_" + id + ".json"
-file_list_of_ints = open(filename, "w")
+file_list_of_ints = open("/tmp/list_of_ints_" + id + ".json", "w")
 file_list_of_ints.write(json.dumps(list_of_ints))
 file_list_of_ints.close()
