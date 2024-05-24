bbf5f07ce3ca6feb4bfdde383ba265ea090a042b
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-24 13:21:31 +0200

    add1-r-dev-user-name-at-domain-com update

--- add1-r-dev-user-name-at-domain-com/task.R
+++ add1-r-dev-user-name-at-domain-com/task.R
@@ -1,30 +1,64 @@
 setwd('/app')
-
-# retrieve input parameters
-
 library(optparse)
 library(jsonlite)
 
 
+
+print('option_list')
 option_list = list(
 
 make_option(c("--count"), action="store", default=NA, type="integer", help="my description"), 
 make_option(c("--id"), action="store", default=NA, type="character", help="my description")
-
 )
 
-# set input parameters accordingly
+
 opt = parse_args(OptionParser(option_list=option_list))
 
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving count")
+var = opt$count
+print(var)
+var_len = length(var)
+print(paste("Variable count has length", var_len))
+
 count = opt$count
-id <- gsub('"', '', opt$id)
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
 
 
-
-
-
+print("Running the cell")
 
 a = count + 1
-
-
-
