10fb1c7a73143ea178a827af6211dd91379f601e
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-24 13:21:36 +0200

    input-lists-r-dev-user-name-at-domain-com update

--- input-lists-r-dev-user-name-at-domain-com/task.R
+++ input-lists-r-dev-user-name-at-domain-com/task.R
@@ -1,26 +1,57 @@
 setwd('/app')
-
-# retrieve input parameters
-
 library(optparse)
 library(jsonlite)
 
 
+
+print('option_list')
 option_list = list(
 
 make_option(c("--id"), action="store", default=NA, type="character", help="my description")
-
 )
 
-# set input parameters accordingly
+
 opt = parse_args(OptionParser(option_list=option_list))
 
-id <- gsub('"', '', opt$id)
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
 
 
-
-
-
+print("Running the cell")
 
 list_of_paths <- c(
   "/webdav/LAZ/targets_myname",
@@ -33,13 +64,12 @@ list_of_ints <- c(1, 2, 35, 6, 65)
 
 print(list_of_paths)
 print(list_of_ints)
-
-
-
 # capturing outputs
+print('Serialization of list_of_paths')
 file <- file(paste0('/tmp/list_of_paths_', id, '.json'))
 writeLines(toJSON(list_of_paths, auto_unbox=TRUE), file)
 close(file)
+print('Serialization of list_of_ints')
 file <- file(paste0('/tmp/list_of_ints_', id, '.json'))
 writeLines(toJSON(list_of_ints, auto_unbox=TRUE), file)
 close(file)
