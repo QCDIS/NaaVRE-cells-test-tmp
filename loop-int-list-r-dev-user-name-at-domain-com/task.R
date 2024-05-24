043f1b4a0a5dbd55183b487e81e7043017f49eef
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-24 13:21:54 +0200

    loop-int-list-r-dev-user-name-at-domain-com update

--- loop-int-list-r-dev-user-name-at-domain-com/task.R
+++ loop-int-list-r-dev-user-name-at-domain-com/task.R
@@ -1,32 +1,70 @@
 setwd('/app')
-
-# retrieve input parameters
-
 library(optparse)
 library(jsonlite)
 
 
+
+print('option_list')
 option_list = list(
 
 make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
 make_option(c("--list_of_ints"), action="store", default=NA, type="character", help="my description")
-
 )
 
-# set input parameters accordingly
+
 opt = parse_args(OptionParser(option_list=option_list))
 
-id <- gsub('"', '', opt$id)
-list_of_ints = fromJSON(opt$list_of_ints)
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+print("Retrieving list_of_ints")
+var = opt$list_of_ints
+print(var)
+var_len = length(var)
+print(paste("Variable list_of_ints has length", var_len))
+
+print("------------------------Running var_serialization for list_of_ints-----------------------")
+print(opt$list_of_ints)
+list_of_ints = var_serialization(opt$list_of_ints)
+print("---------------------------------------------------------------------------------")
 
 
 
-
-
+print("Running the cell")
 
 for (l in list_of_ints) {
     print(l)
 }
-
-
-
