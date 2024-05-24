27a8aaa4dc09b72f3651c6534d755051d5bd2113
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-24 13:21:22 +0200

    read-file-lines-r-dev-user-name-at-domain-com update

--- read-file-lines-r-dev-user-name-at-domain-com/task.R
+++ read-file-lines-r-dev-user-name-at-domain-com/task.R
@@ -1,29 +1,66 @@
 setwd('/app')
-
-# retrieve input parameters
-
 library(optparse)
 library(jsonlite)
 
 
+
+print('option_list')
 option_list = list(
 
 make_option(c("--file_path"), action="store", default=NA, type="character", help="my description"), 
 make_option(c("--id"), action="store", default=NA, type="character", help="my description")
-
 )
 
-# set input parameters accordingly
+
 opt = parse_args(OptionParser(option_list=option_list))
 
-file_path <- gsub('"', '', opt$file_path)
-id <- gsub('"', '', opt$id)
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
 
+print("Retrieving file_path")
+var = opt$file_path
+print(var)
+var_len = length(var)
+print(paste("Variable file_path has length", var_len))
 
+file_path <- gsub("\"", "", opt$file_path)
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
 
 conf_data_folder <- file.path('/tmp', 'data')
 
-
+print("Running the cell")
 conf_data_folder <- file.path('/tmp', 'data')
 
 onlyfiles <- list.files(conf_data_folder, full.names = TRUE)
@@ -33,10 +70,8 @@ print(onlyfiles)
 f <- file(file_path, "r")
 lines <- readLines(f)
 close(f)
-
-
-
 # capturing outputs
+print('Serialization of lines')
 file <- file(paste0('/tmp/lines_', id, '.json'))
 writeLines(toJSON(lines, auto_unbox=TRUE), file)
 close(file)
