2f4a73aadc67ccbb868e4f1f5380f478d68d244d
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-28 19:18:11 +0200

    use-secret-r-user creation

---
+++ use-secret-r-user/task.R
@@ -0,0 +1,65 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+
+secret_xyz = Sys.getenv('secret_xyz')
+
+print('option_list')
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--param_xyz"), action="store", default=NA, type="character", help="my description")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+print("Retrieving param_xyz")
+var = opt$param_xyz
+print(var)
+var_len = length(var)
+print(paste("Variable param_xyz has length", var_len))
+
+param_xyz <- gsub("\"", "", opt$param_xyz)
+
+
+print("Running the cell")
+print(param_xyz)
+print(secret_xyz)
