66cad2e361fdc7be8e3eb99895727a590070d5b4
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-28 18:57:47 +0200

    untitled-user update

--- untitled-user/task.py
+++ untitled-user/task.py
@@ -6,7 +6,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--param_xyz', action='store', type=int, required=True, dest='param_xyz')
+arg_parser.add_argument('--param_xyz', action='store', type=str, required=True, dest='param_xyz')
 
 args = arg_parser.parse_args()
 print(args)
@@ -14,7 +14,7 @@ print(args)
 id = args.id
 
 
-param_xyz = args.param_xyz
+param_xyz = args.param_xyz.replace('"','')
 
 
 print(param_xyz, secret_xyz)
