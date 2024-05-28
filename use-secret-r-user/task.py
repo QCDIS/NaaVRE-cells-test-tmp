6ef36aaa9c33adaac78cc0419a2348a0a97eebd5
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-28 19:15:45 +0200

    use-secret-r-user creation

---
+++ use-secret-r-user/task.py
@@ -0,0 +1,25 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+  secret_xyz = os.getenv('secret_xyz')
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--param_xyz', action='store', type=str, required=True, dest='param_xyz')
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+param_xyz = args.param_xyz.replace('"','')
+
+
+print(param_xyz)
+print(secret_xyz)
+
