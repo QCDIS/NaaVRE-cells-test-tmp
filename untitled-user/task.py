00ee4591b2c3331942722796ea98c88b5744b13f
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-28 12:03:11 +0200

    untitled-user creation

---
+++ untitled-user/task.py
@@ -0,0 +1,21 @@
+
+import argparse
+import json
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--param_xyz', action='store', type=int, required=True, dest='param_xyz')
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+param_xyz = args.param_xyz
+
+
+print(param_xyz, secret_xyz)
+
