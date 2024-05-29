170ae91115e99edcbed6d641b1e0b1e4dbb5f742
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-29 13:29:00 +0200

    create-file-dev-user-name-at-domain-com update

--- create-file-dev-user-name-at-domain-com/task.py
+++ create-file-dev-user-name-at-domain-com/task.py
@@ -32,6 +32,7 @@ fp.close()
 onlyfiles = [f for f in listdir(conf_data_folder) if isfile(join(conf_data_folder, f))]
 
 print(onlyfiles)
+a = 0.1374939992425145
 
 file_file_path = open("/tmp/file_path_" + id + ".json", "w")
 file_file_path.write(json.dumps(file_path))
