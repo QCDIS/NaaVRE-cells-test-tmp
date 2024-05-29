7fc2a9a3ab61fa7af577b4168fc249aea4c74e0e
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-29 14:01:29 +0200

    vol2bird-dev-user-name-at-domain-com update

--- vol2bird-dev-user-name-at-domain-com/task.py
+++ vol2bird-dev-user-name-at-domain-com/task.py
@@ -24,8 +24,6 @@ cmd = "vol2bird --version"
 
 msg = os.system(cmd)  # returns the exit code in unix
 
-a = 0.0049119747306544115
-
 file_msg = open("/tmp/msg_" + id + ".json", "w")
 file_msg.write(json.dumps(msg))
 file_msg.close()
