9b6fbd171e8f2dc0985c4603844a48a68443d4d2
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-29 14:16:00 +0200

    loop-file-lines-r-dev-user-name-at-domain-com update

--- loop-file-lines-r-dev-user-name-at-domain-com/task.R
+++ loop-file-lines-r-dev-user-name-at-domain-com/task.R
@@ -69,6 +69,7 @@ for (l in lines) {
     count <- count + 1
     cat(sprintf("Line %d: %s\n", count, trimws(l)))
 }
+a = 0.2974968611192097
 # capturing outputs
 print('Serialization of count')
 file <- file(paste0('/tmp/count_', id, '.json'))
