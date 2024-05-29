64da6732c1f653f6278ddfe13a7a4dcac8718a70
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-29 13:28:04 +0200

    create-file-r-dev-user-name-at-domain-com update

--- create-file-r-dev-user-name-at-domain-com/task.R
+++ create-file-r-dev-user-name-at-domain-com/task.R
@@ -63,6 +63,7 @@ writeLines(L, file_path)
 onlyfiles <- list.files(conf_data_folder, full.names = TRUE)
 
 print(onlyfiles)
+a = 0.46892034879364763
 # capturing outputs
 print('Serialization of file_path')
 file <- file(paste0('/tmp/file_path_', id, '.json'))
