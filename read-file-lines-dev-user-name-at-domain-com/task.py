2b832dd6e9468dbe2d141302486317765532f812
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-29 13:30:29 +0200

    read-file-lines-dev-user-name-at-domain-com update

--- read-file-lines-dev-user-name-at-domain-com/task.py
+++ read-file-lines-dev-user-name-at-domain-com/task.py
@@ -33,6 +33,7 @@ print(onlyfiles)
 f = open(file_path, 'r')
 lines = f.readlines()
 f.close()
+a = 0.9549753599316206
 
 file_lines = open("/tmp/lines_" + id + ".json", "w")
 file_lines.write(json.dumps(lines))
