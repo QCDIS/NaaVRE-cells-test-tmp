85bce81bfb0629b3175bc3cf480ca8e304e478ea
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-29 14:01:52 +0200

    loop-file-lines-dev-user-name-at-domain-com update

--- loop-file-lines-dev-user-name-at-domain-com/task.py
+++ loop-file-lines-dev-user-name-at-domain-com/task.py
@@ -23,7 +23,6 @@ count = 0
 for l in lines:
     count += 1
     print("Line{}: {}".format(count, l.strip()))
-a = 0.5876463648433824
 
 file_count = open("/tmp/count_" + id + ".json", "w")
 file_count.write(json.dumps(count))
