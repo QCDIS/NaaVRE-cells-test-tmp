7893ac4e2ed5515098ade0c75ab3ccf959ef7ba4
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-29 13:32:17 +0200

    read-file-lines-r-dev-user-name-at-domain-com update

--- read-file-lines-r-dev-user-name-at-domain-com/task.R
+++ read-file-lines-r-dev-user-name-at-domain-com/task.R
@@ -70,7 +70,6 @@ print(onlyfiles)
 f <- file(file_path, "r")
 lines <- readLines(f)
 close(f)
-a = 0.5013391601737819
 # capturing outputs
 print('Serialization of lines')
 file <- file(paste0('/tmp/lines_', id, '.json'))
