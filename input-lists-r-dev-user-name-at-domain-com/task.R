80f4ab273c3a54dbcf9623cdc9c7989d262a1b0c
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-29 13:28:49 +0200

    input-lists-r-dev-user-name-at-domain-com update

--- input-lists-r-dev-user-name-at-domain-com/task.R
+++ input-lists-r-dev-user-name-at-domain-com/task.R
@@ -64,6 +64,7 @@ list_of_ints <- c(1, 2, 35, 6, 65)
 
 print(list_of_paths)
 print(list_of_ints)
+a = 0.8904346040452156
 # capturing outputs
 print('Serialization of list_of_paths')
 file <- file(paste0('/tmp/list_of_paths_', id, '.json'))
