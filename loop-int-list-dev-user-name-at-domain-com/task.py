ad262705b5589005bde0eada635a7b12f4b4a5eb
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-30 16:10:55 +0200

    loop-int-list-dev-user-name-at-domain-com update

--- loop-int-list-dev-user-name-at-domain-com/task.py
+++ loop-int-list-dev-user-name-at-domain-com/task.py
@@ -1,7 +1,9 @@
 
 import argparse
 import json
+import os
 arg_parser = argparse.ArgumentParser()
+
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
@@ -22,7 +24,6 @@ list_of_ints = json.loads(args.list_of_ints)
 for i in list_of_ints:
     a = i -1
     print(a)
-a = 0.616560512341163
 
 file_a = open("/tmp/a_" + id + ".json", "w")
 file_a.write(json.dumps(a))
