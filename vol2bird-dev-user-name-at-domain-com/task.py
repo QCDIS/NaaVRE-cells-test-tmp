3b41f2e9a61da35b8a67a149ea62a5e1220b0711
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-30 16:10:26 +0200

    vol2bird-dev-user-name-at-domain-com update

--- vol2bird-dev-user-name-at-domain-com/task.py
+++ vol2bird-dev-user-name-at-domain-com/task.py
@@ -2,7 +2,9 @@ import os
 
 import argparse
 import json
+import os
 arg_parser = argparse.ArgumentParser()
+
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
@@ -23,8 +25,6 @@ print(a)
 cmd = "vol2bird --version"
 
 msg = os.system(cmd)  # returns the exit code in unix
-
-a = 0.4917223932974123
 
 file_msg = open("/tmp/msg_" + id + ".json", "w")
 file_msg.write(json.dumps(msg))
