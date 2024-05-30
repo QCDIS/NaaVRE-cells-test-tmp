97eb67f2bd0383d6694ffb8ab9a0915288f947ef
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-05-30 16:11:07 +0200

    knmi-vol-h5-to-odim-h5-dev-user-name-at-domain-com update

--- knmi-vol-h5-to-odim-h5-dev-user-name-at-domain-com/task.py
+++ knmi-vol-h5-to-odim-h5-dev-user-name-at-domain-com/task.py
@@ -2,7 +2,9 @@ import os
 
 import argparse
 import json
+import os
 arg_parser = argparse.ArgumentParser()
+
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
@@ -23,7 +25,6 @@ print(a)
 cmd = "KNMI_vol_h5_to_ODIM_h5 "
 
 msg = os.system(cmd)  # returns the exit code in unix
-a = 0.33032435059898524
 
 file_msg = open("/tmp/msg_" + id + ".json", "w")
 file_msg.write(json.dumps(msg))
