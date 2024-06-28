83321290268d33b507052ca6d360db121f8dbb2e
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-06-28 10:14:45 +0200

    unique-cell-dev-user-name-domain-com creation

---
+++ unique-cell-dev-user-name-domain-com/task.py
@@ -0,0 +1,24 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--a', action='store', type=int, required=True, dest='a')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+a = args.a
+
+
+
+print(a)
+
